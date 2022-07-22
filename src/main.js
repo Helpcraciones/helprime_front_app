19a8fd693d3e312c08662fdb21898727adb48a97
JuanManrique <juansanchezmanrique@gmail.com>
2022-07-22 03:28:36 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -6,4 +6,5 @@ import store from './store'
 import './styles/app.css';
 import 'flowbite';
 
+
 createApp(App).use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -48,6 +48,7 @@ export default {
       /* this.$store.commit("changeRegistered")
       this.$store.commit('userCurrent', null) */
     },
+
   },
 };
 </script>
