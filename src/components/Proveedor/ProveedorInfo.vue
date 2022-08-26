0495da4a569ab638ada3967d19d0f503c1a0d23b
Maicol Rodriguez <maiikol1192@gmail.com>
2022-08-26 10:21:38 -0500

    cambiosFor

--- src/components/Proveedor/ProveedorInfo.vue
+++ src/components/Proveedor/ProveedorInfo.vue
@@ -255,6 +255,7 @@ export default {
                 img.url = signedURL
               }
             });
+            
             if(error) throw error;
           }
         this.loader = false 
