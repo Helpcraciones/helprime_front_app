08455f9763f3497158d78b34ea007b29db349838
Maicol Rodriguez <maiikol1192@gmail.com>
2022-08-26 09:37:58 -0500

    cambiosSinNull

--- src/components/Proveedor/ProveedorInfo.vue
+++ src/components/Proveedor/ProveedorInfo.vue
@@ -154,7 +154,7 @@ export default {
       currentInfo : "",
       imgs: [],
       currentClass: "",
-      toggleVer: undefined,
+      toggleVer: "",
       modalGalery : false,
     // carousel settings
     settings: {
