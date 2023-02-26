1233fc848874cb5c2622ee8e946b4ed58b29692a
Maicol Rodriguez <maiikol1192@gmail.com>
2023-02-26 18:14:58 +0100

    cambios para dist

--- vue.config.js
+++ vue.config.js
@@ -22,74 +22,69 @@ module.exports = defineConfig({
     manifestOptions: {
       icons: [
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431272/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_1_s8nyqx.svg",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431313/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_2_hwth6x.svg",
           sizes: "512x512",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431272/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_1_s8nyqx.svg",
           sizes: "192x192",
           type: "image/png",
           purpose: "maskable",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431313/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_2_hwth6x.svg",
           sizes: "512x512",
           type: "image/png",
           purpose: "maskable",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431373/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_3_f8nlcw.svg",
           sizes: "60x60",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431403/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_4_wjble4.svg",
           sizes: "76x76",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431437/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_5_vzcsth.svg",
           sizes: "120x120",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431483/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_6_oejqfi.svg",
           sizes: "152x152",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431512/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_7_ft7iu8.svg",
           sizes: "180x180",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
-          sizes: "180x180",
-          type: "image/png",
-        },
-        {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431558/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_8_ckeozc.svg",
           sizes: "16x16",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431593/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_9_ey3lhl.svg",
           sizes: "32x32",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431632/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_10_ma4ou8.svg",
           sizes: "144x144",
           type: "image/png",
         },
         {
-          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677427165/APP/ICON_Mesa_de_trabajo_1_tnkmtc-_4__oorfez.png",
+          src: "https://res.cloudinary.com/vital-seguros/image/upload/v1677431663/APP/ICON_Mesa_de_trabajo_1_tnkmtc_4_11_qve5he.svg",
           sizes: "150x150",
           type: "image/png",
         },
