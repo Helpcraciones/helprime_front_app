e689cb9112b13a6098cdd0d9011b6762d76fde7b
Maicol Rodriguez <maiikol1192@gmail.com>
2023-02-26 16:57:32 +0100

    cambios para dist

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,18 @@ module.exports = defineConfig({
   pwa: {
     name: 'HELPRIME ',
     themeColor: '#fff',
-    msTileColor: '#000',
+    msTileColor: '#fff',
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
       skipWaiting: true,
     },
     iconPaths: {
+      faviconSVG: 'https://res.cloudinary.com/vital-seguros/image/upload/v1656450064/APP/ICON_Mesa_de_trabajo_1_tnkmtc.svg',
+      favicon32: 'https://res.cloudinary.com/vital-seguros/image/upload/v1656450064/APP/ICON_Mesa_de_trabajo_1_tnkmtc.svg',
+      favicon16: 'https://res.cloudinary.com/vital-seguros/image/upload/v1656450064/APP/ICON_Mesa_de_trabajo_1_tnkmtc.svg',
+      appleTouchIcon: 'https://res.cloudinary.com/vital-seguros/image/upload/v1656450064/APP/ICON_Mesa_de_trabajo_1_tnkmtc.svg',
+      maskIcon: 'https://res.cloudinary.com/vital-seguros/image/upload/v1656450064/APP/ICON_Mesa_de_trabajo_1_tnkmtc.svg',
+      msTileImage: 'https://res.cloudinary.com/vital-seguros/image/upload/v1656450064/APP/ICON_Mesa_de_trabajo_1_tnkmtc.svg',
       msTileImage: 'https://res.cloudinary.com/vital-seguros/image/upload/v1656450064/APP/ICON_Mesa_de_trabajo_1_tnkmtc.svg'
     }
   },
